76fef7d0b70bf14d14e6f420e776212ed3b50ef7
Festorah <festusoyebami@gmail.com>
2024-10-07 13:34:46 +0100

    users api update

--- api/serializers/users.py
+++ api/serializers/users.py
@@ -6,9 +6,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountUser
         fields = ["email_address", "password", "first_name", "last_name"]
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+            },
+        }
 
     def create(self, validated_data):
         user = AccountUser(**validated_data)
         user.set_password(validated_data["password"])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
+            validated_data.pop("password")
+
+        return super().update(instance, validated_data)
